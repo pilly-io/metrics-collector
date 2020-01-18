287d10e553b80cd63325a63f0da885da481e9899
py_david <david.macro@volvocars.com>
2020-01-18 12:10:00 +0100

    move logging init() to main()

--- cmd/kube-metadata-collector/main.go
+++ cmd/kube-metadata-collector/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"os"
 	log "github.com/sirupsen/logrus"
+	"github.com/pilly-io/metrics-collector/internal/kubernetes"
 )
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetReportCaller(true)
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
 	//1. Get config
 	config := GetConfig()
 	log.Info(config)
 	//2. Connect to the db
-	db := NewDb(config.DbURI)
-	log.Info(db)
+	//db := NewDb(config.DbURI)
+	//log.Info(db)
 	//3. Initialize Kubernetes API
 	kubernetesClient := NewKubernetesClient()
 	log.Info(kubernetesClient)
